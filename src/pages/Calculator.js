d861f8a8366c8c712d335a810b978ff100226ec2
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2023-03-29 00:37:02 +0300

    made changes

--- src/pages/Calculator.js
+++ src/pages/Calculator.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Calculator from '../components/Calculator';
-import Qoute from '../components/qoute';
 import './style.css';
 
 const CalculatorPage = () => (
   <div className="page_container">
     <div className="qoute_wrapper">
-      <Qoute />
+      <h1>Let&rsquo;s Do Some Math!</h1>
     </div>
     <div className="digit_wrapper">
       <Calculator />
